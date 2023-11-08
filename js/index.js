dc9ee1d471ae123afe0366bfdb72e02e8f6abf81
gaston-conte <gastonconte1@hotmail.com>
2023-11-08 14:19:28 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function Limpiar() {
+    document.getElementsById('Formulario').reset();
+}
+
+function sumar(num) {
+    let x = parseInt(document.getElementById('valor1'));
+    let y = parseInt(document.getElementById('valor2'));
+    alert(x+y);
+}
